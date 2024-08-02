d19d7e669edd4ba037aa424ce0a9fa3c95d09381
Umair ul Islam <umairulislam09@gmail.com>
2024-08-02 18:32:04 +0500

    vercel file added

--- index.js
+++ index.js
@@ -406,6 +406,8 @@ passport.deserializeUser(async (id, cb) => {
   }
 })
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`)
+// })
+
+export default app
