155212723ce25b4aa5771bc83f438afffa55b964
Umair ul Islam <umairulislam09@gmail.com>
2024-08-05 17:41:44 +0500

    Supabase configuration

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Necessary for Supabase
+  },
 })
 
 // Connect to PostgreSQL databas
